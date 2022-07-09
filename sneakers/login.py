d3851ea1c4c2f3b280a4ba03541a5a024886a8cf
Bhanu Prakash <77429972+bhanu1031@users.noreply.github.com>
2022-07-09 21:08:39 +0530

    Add files via upload
    Frontend of a sneaker and streetwear selling platform

---
+++ sneakers/login.py
@@ -0,0 +1,14 @@
+#Sign up
+first_name= input('Enter First Name: ')
+last_name= input('Enter Last Name: ')
+username= input('Enter Username: ')
+password= input('Enter Password: ')
+print('Registration Successful!')
+#login
+username1= input('Enter Username: ')
+password1= input('Enter Password')
+if username == username1 and password == password1:
+    print('Login Successful')
+else:
+    print('Invalid credentials')
+    
